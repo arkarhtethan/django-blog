03d0b5974ea3ab4ba068754cdd863f050a842976
Kaung Myat Han <arkarhtethan@gmail.com>
2019-05-04 23:01:04 +0630

    Authentication feature added to api

--- posts/routers.py
+++ posts/routers.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .viewsets import PostViewSet
+from .viewsets import PostViewSet, LoginViewSet
 
 router = DefaultRouter()
 
 router.register("post-view-set", PostViewSet)
+router.register("login-view-set", LoginViewSet, base_name="login-view-set")
--- posts/viewsets.py
+++ posts/viewsets.py
@@ -1,12 +1,25 @@
-from .serializers import PostSerailzier
 from .models import Post
+from .serializers import PostSerailzier
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework import mixins
-from rest_framework import generics
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class PostViewSet(viewsets.ModelViewSet):
+
+    authentication_classes = (TokenAuthentication,)
+
+    permission_classes = (IsAuthenticated,)
 
     serializer_class = PostSerailzier
 
     queryset = Post.objects.all()
+
+class LoginViewSet(viewsets.ViewSet):
+
+    def create(self, request):
+
+        return ObtainAuthToken().post(request)
+    
